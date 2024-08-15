5d09fd52869c8515436cbd6b5eecea8c1e1eb6d7
IdowuMicheal-1 <idowumicheal506@gmail.com>
2024-08-15 15:34:49 +0100

    'update'

--- src/components/RuleBtn.js
+++ src/components/RuleBtn.js
@@ -10,9 +10,9 @@ const RuleBtn = () => {
   }
   
   return (
-        <div className='mt-16 mx-auto md:ml-[90%] lg:mt-6'>
-            <div className=' bg-radialGradient border border-white rounded-md max-w-24 px-4 py-2 text-center cursor-pointer'>
-            <p className='font-serif uppercase text-sm text-white font-semibold' onClick={setRuleHandler} >Rules</p>
+        <div className='mt-16 mx-auto md:ml-[90%] lg:mt-6 group'>
+            <div className=' bg-radialGradient border border-white rounded-md max-w-24 px-4 py-2 text-center group-hover:cursor-pointer'>
+            <p className='font-serif uppercase text-sm text-white font-semibold w-full' onClick={setRuleHandler} >Rules</p>
             </div>
         </div>
   )
